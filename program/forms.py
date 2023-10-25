db10523e55f7689a7cb7d313e9efa1d944fec895
pemochamdev <pemochamdev@gmail.com>
2023-10-25 05:27:12 +0000

    creation des formulaires ResponseForm, CommentForm

--- program/forms.py
+++ program/forms.py
@@ -1,9 +1,42 @@
 from django import forms
 
-from program.models import Lesson, Subject, Level
+from program.models import (
+    Comment,
+    Lesson, 
+    Level,
+    Response,
+    Subject, 
+)
 
 class LessonForm(forms.ModelForm):
     
     class Meta:
         model = Lesson
         fields = ( 'position', 'name', 'video','fichier_pdf', 'fiche_presentation')
+
+class CommentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Comment
+        fields = ("content",)
+        widget = {
+            'content': forms.Textarea(attrs={
+                'placeholder':'add your comment here',
+                'rows':4,
+                'cols':70
+            })
+        }
+
+
+class ResponseForm(forms.ModelForm):
+    
+    class Meta:
+        model = Response
+        fields = ("content",)
+        widget = {
+            'content': forms.Textarea(attrs={
+                'placeholder':'add your response here',
+                'rows':3,
+                'cols':50
+            })
+        }
